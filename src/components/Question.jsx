69a14826ff25128afb75a63cf42d237a26d32699
Alexander Marini <svenskost6@gmail.com>
2024-03-21 23:02:02 +0100

    progress

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,16 +2,15 @@ import PropTypes from 'prop-types'
 import Option from './Option'
 
 function Question({ data, handleSubmit }) {
+   console.log(data.options)
    return (
-      <main className='relative bg-white px-6 pb-8 pt-10 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10'>
-         <div className='mx-auto max-w-md'>
-            <h1>{data.question}</h1>
-            <img src={`src/assets/${data.img}`} />
-            {data.options.map((option) => {
-               <Option key={option.id} text={option.text} />
-            })}
-         </div>
-      </main>
+      <>
+         <h1>{data.question}</h1>
+         <img src={`src/assets/${data.img}`} />
+         {data.options.map((option) => {
+            <Option key={option.id} text={option.text} />
+         })}
+      </>
    )
 }
 
